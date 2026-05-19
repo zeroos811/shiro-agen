e7571900973ea4d48fb9a51aab32a8ca9299959c
zeroos811 <68334036+zeroos811@users.noreply.github.com>
2026-05-19 21:25:59 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'shiro-clean-v2';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES).catch(function(){});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(function(response) {
+        var clone = response.clone();
+        caches.open(CACHE).then(function(cache){
+          cache.put(e.request, clone);
+        });
+        return response;
+      })
+      .catch(function() {
+        return caches.match(e.request);
+      })
+  );
+});
